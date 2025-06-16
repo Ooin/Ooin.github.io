11afcef1669b78e3872fb2c0df263550c6e08741
Ooin <xaxipistaxi@gmail.com>
2025-06-16 10:11:02 +0200

    Revert "Base page implemented"
    This reverts commit b097900c1feac3f4224e9156a0081a8f1a29c468.

--- ReactMigration/src/Components/CoolButton.jsx
+++
@@ -1,9 +0,0 @@
-const CoolButton = ({ linkToContent , linkText }) =>{
-    return (
-        <>
-            <a href={linkToContent}>{linkText}</a>
-        </>
-    )
-}
-
-export default CoolButton;
